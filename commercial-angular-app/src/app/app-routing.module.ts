35d907bed05d5af8fa540df6db2e7547827376ae
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2023-11-15 08:12:04 +0200

    angular client

--- commercial-angular-app/src/app/app-routing.module.ts
+++ commercial-angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { CardComponent } from './components/card/card.component';
+import { ProductComponent } from './components/product/product.component';
+import { ViewArticleComponent } from './components/view-article/view-article.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'view-article-detail/:id', component: ViewArticleComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'card', component: CardComponent},
+  {path: 'product', component: ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- commercial-angular-app/src/app/app.module.ts
+++ commercial-angular-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewArticleComponent } from './components/view-article/view-article.component';
+import { LoginComponent } from './components/login/login.component';
+import { CardComponent } from './components/card/card.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewArticleComponent,
+    LoginComponent,
+    CardComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
